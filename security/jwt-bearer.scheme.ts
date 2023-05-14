4bb2c88daad67e582b11d09cc59c0ad3628ab02b
Monkey Scorpion <monkeyscorpionratdev@gmail.com>
2023-05-14 11:29:32 +0700

    merge main and resolve conflict

--- security/jwt-bearer.scheme.ts
+++ security/jwt-bearer.scheme.ts
@@ -27,12 +27,8 @@ const DAYS_30 = 30 * 24 * 60 * 60 * 1000;
 
 const AuthorizationHeader = 'Authorization';
 const AcceptHeader = 'Accept';
-<<<<<<< HEAD
-const accepts = ['*/*', 'application/json'];
-=======
 const AcceptTypeJSON = 'application/json';
 const AcceptTypeAny = '*/*';
->>>>>>> origin/main
 
 export class JwtBearerScheme implements AuthenticationScheme {
   constructor(
@@ -46,24 +42,11 @@ export class JwtBearerScheme implements AuthenticationScheme {
     const headers = context.request.serverRequest.request.headers;
 
     const headAuthorization = headers.get(AuthorizationHeader);
-<<<<<<< HEAD
-
-    // TODO: should we really do content negotiation here? need to fill an issue at alosaur
-    const headAccept = (headers.get(AcceptHeader) || '*/*')
-      .split(',')
-      .map((x) => x.trim());
-
-    const shouldAccept = headAccept
-      .some((acceptType) => accepts.includes(acceptType));
-
-    if (shouldAccept && headAuthorization) {
-=======
     const headAccept = headers.get(AcceptHeader) || '';
 
     if (
       [AcceptTypeJSON, AcceptTypeAny].includes(headAccept) && headAuthorization
     ) {
->>>>>>> origin/main
       const token = getBearerToken(headAuthorization);
 
       if (token) {

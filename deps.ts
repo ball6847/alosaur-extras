13db51e305d552d6e19a48af2801f073d74d5dc0
Monkey Scorpion <monkeyscorpionratdev@gmail.com>
2023-06-20 16:13:16 +0700

    feat(query-parser): support date filtering, ISO, SQL or unixtimestamp

--- deps.ts
+++ deps.ts
@@ -1,29 +1,25 @@
-export { ApiProperty } from 'https://cdn.jsdelivr.net/gh/ball6847/alosaur-openapi@0.1.4/mod.ts';
+export { ApiProperty, ApiQuery } from 'https://cdn.jsdelivr.net/gh/ball6847/alosaur-openapi@0.1.4/mod.ts';
 export {
-  BusinessType, Content, HttpContext,
-  Singleton, container, getMetadataArgsStorage, type ActionResult, type HookTarget
+  type ActionResult,
+  BusinessType,
+  container,
+  Content,
+  getMetadataArgsStorage,
+  type HookTarget,
+  HttpContext,
+  Singleton,
 } from 'https://deno.land/x/alosaur@v0.38.0/mod.ts';
 export { type default as InjectionToken } from 'https://deno.land/x/alosaur@v0.38.0/src/injection/providers/injection-token.ts';
 export { defineInjectionTokenMetadata } from 'https://deno.land/x/alosaur@v0.38.0/src/injection/reflection-helpers.ts';
 export {
   type AuthenticationScheme,
-  type Identity
+  type Identity,
 } from 'https://deno.land/x/alosaur@v0.38.0/src/security/authentication/core/auth.interface.ts';
 export { type AuthPolicy } from 'https://deno.land/x/alosaur@v0.38.0/src/security/authorization/mod.ts';
 export { SecurityContext } from 'https://deno.land/x/alosaur@v0.38.0/src/security/context/security-context.ts';
 export { memoizy } from 'https://deno.land/x/memoizy@1.0.0/mod.ts';
-export {
-  plainToInstance, type ClassConstructor
-} from 'https://esm.sh/v115/class-transformer@0.5.1';
-export {
-  ValidationError, validate
-} from 'https://esm.sh/v115/class-validator@0.14.0';
+export { type ClassConstructor, plainToInstance } from 'https://esm.sh/v115/class-transformer@0.5.1';
+export { validate, ValidationError } from 'https://esm.sh/v115/class-validator@0.14.0';
 export { default as humanizeString } from 'https://esm.sh/v115/humanize-string@3.0.0';
-export {
-  merge,
-  omit,
-  omitBy,
-  pick,
-  pickBy
-} from 'https://esm.sh/v115/midash@0.8.2';
-
+export { DateTime } from 'https://esm.sh/v115/luxon@3.3.0';
+export { merge, omit, omitBy, pick, pickBy } from 'https://esm.sh/v115/midash@0.8.2';
--- query_parser_hook.ts
+++ query_parser_hook.ts
@@ -1,5 +1,6 @@
-import { HookTarget, HttpContext } from './deps.ts';
-import { EntityContext, MetaData, parseQueryString } from './query_parser.ts';
+import { UseHook } from 'https://deno.land/x/alosaur@v0.38.0/mod.ts';
+import { ApiQuery, HookTarget, HttpContext } from './deps.ts';
+import { EntityContext, getDefaultContext, MetaData, parseQueryString } from './query_parser.ts';
 
 export type QueryState = {
   metadata: Required<MetaData>;
@@ -16,4 +17,74 @@ export class QueryParserHook implements HookTarget<QueryState, EntityContext> {
     );
     context.state.metadata = metadata as Required<MetaData>;
   }
+}
+
+function getFieldTypeFunc(type: string) {
+  switch (type) {
+    case 'number':
+      return Number;
+    case 'boolean':
+      return Boolean;
+    case 'string':
+      return String;
+    case 'date': // date string (ISO 8601 or mysql date ?)
+      return String;
+    case 'unix': // unix timestamp seconds
+      return Number;
+    default:
+      return String;
+  }
+}
+
+export function QueryParser(payload: EntityContext) {
+  // deno-lint-ignore no-explicit-any
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const config = getDefaultContext(payload);
+    // automatically register all needed decorators
+    UseHook(QueryParserHook, config)(target, propertyKey, descriptor);
+
+    if (config.openapi) {
+      // page
+      ApiQuery({
+        name: 'page',
+        type: Number,
+        // @ts-ignore upgrade to latest version of alosaur-openapi
+        default: 1,
+        required: false,
+      })(target, propertyKey, descriptor);
+
+      // limit
+      // TODO: check maxLimit and allowNoLimit to generate description
+      ApiQuery({
+        name: 'limit',
+        type: Number,
+        // @ts-ignore upgrade to latest version of alosaur-openapi
+        default: config.defaultLimit,
+        required: false,
+      })(target, propertyKey, descriptor);
+
+      // filterable fields
+      for (const key in config.fields) {
+        const field = config.fields[key];
+
+        if (field.filterable) {
+          if (field.enum) {
+            ApiQuery({
+              name: key,
+              enum: field.enum ? field.enum : undefined,
+              required: false,
+              description: field.description,
+            })(target, propertyKey, descriptor);
+          } else {
+            ApiQuery({
+              name: key,
+              type: getFieldTypeFunc(field.type),
+              required: false,
+              description: field.description,
+            })(target, propertyKey, descriptor);
+          }
+        }
+      }
+    }
+  };
 }

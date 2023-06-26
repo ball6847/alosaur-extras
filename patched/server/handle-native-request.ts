67871d0074a2fb146f6b20be2a138460c6485f0d
Monkey Scorpion <monkeyscorpionratdev@gmail.com>
2023-06-26 19:24:56 +0700

    fix: make serve handler run coreHook before responding

--- patched/server/handle-native-request.ts
+++ patched/server/handle-native-request.ts
@@ -1,17 +1,18 @@
-import { HttpError } from "https://deno.land/x/alosaur@v0.38.0/src/http-error/HttpError.ts";
-import { MetadataArgsStorage } from "https://deno.land/x/alosaur@v0.38.0/src/metadata/metadata.ts";
-import { MiddlewareMetadataArgs } from "https://deno.land/x/alosaur@v0.38.0/src/metadata/middleware.ts";
-import { App } from "https://deno.land/x/alosaur@v0.38.0/src/mod.ts";
-import { HttpContext } from "https://deno.land/x/alosaur@v0.38.0/src/models/http-context.ts";
-import { PrimitiveResponse } from "https://deno.land/x/alosaur@v0.38.0/src/models/response.ts";
-import { SERVER_REQUEST } from "https://deno.land/x/alosaur@v0.38.0/src/models/tokens.model.ts";
-import { Content } from "https://deno.land/x/alosaur@v0.38.0/src/renderer/content.ts";
-import { notFoundAction } from "https://deno.land/x/alosaur@v0.38.0/src/renderer/not-found.ts";
-import { getActionParams } from "https://deno.land/x/alosaur@v0.38.0/src/route/get-action-params.ts";
-import { getAction } from "https://deno.land/x/alosaur@v0.38.0/src/route/get-action.ts";
-import { getHooksFromAction } from "https://deno.land/x/alosaur@v0.38.0/src/route/get-hooks.ts";
-import { getStaticFile } from "https://deno.land/x/alosaur@v0.38.0/src/utils/get-static-file.ts";
-import { hasHooks, hasHooksAction, resolveHooks } from "https://deno.land/x/alosaur@v0.38.0/src/utils/hook.utils.ts";
+import { HttpError } from 'https://deno.land/x/alosaur@v0.38.0/src/http-error/HttpError.ts';
+import { MetadataArgsStorage } from 'https://deno.land/x/alosaur@v0.38.0/src/metadata/metadata.ts';
+import { MiddlewareMetadataArgs } from 'https://deno.land/x/alosaur@v0.38.0/src/metadata/middleware.ts';
+import { App } from 'https://deno.land/x/alosaur@v0.38.0/src/mod.ts';
+import { HttpContext } from 'https://deno.land/x/alosaur@v0.38.0/src/models/http-context.ts';
+import { PrimitiveResponse } from 'https://deno.land/x/alosaur@v0.38.0/src/models/response.ts';
+import { SERVER_REQUEST } from 'https://deno.land/x/alosaur@v0.38.0/src/models/tokens.model.ts';
+import { Content } from 'https://deno.land/x/alosaur@v0.38.0/src/renderer/content.ts';
+import { notFoundAction } from 'https://deno.land/x/alosaur@v0.38.0/src/renderer/not-found.ts';
+import { getActionParams } from 'https://deno.land/x/alosaur@v0.38.0/src/route/get-action-params.ts';
+import { getAction } from 'https://deno.land/x/alosaur@v0.38.0/src/route/get-action.ts';
+import { getHooksFromAction } from 'https://deno.land/x/alosaur@v0.38.0/src/route/get-hooks.ts';
+import { getStaticFile } from 'https://deno.land/x/alosaur@v0.38.0/src/utils/get-static-file.ts';
+import { hasHooks, hasHooksAction } from 'https://deno.land/x/alosaur@v0.38.0/src/utils/hook.utils.ts';
+import { resolveHooks } from './hooks.ts';
 
 // Get middlewares in request
 function getMiddlewareByUrl<T>(
@@ -51,7 +52,6 @@ async function runCorePostRequestMiddlewares(middlewares: MiddlewareMetadataArgs
   }
 }
 
-
 /**
  * Gets deno native http bindings
  * lite server without: static config, catch requests
@@ -77,8 +77,8 @@ function respondWithWrapper(
   respondWith: (r: Response | Promise<Response>) => Promise<void>,
   conn: Deno.Conn,
 ): (r: Response | Promise<Response>) => Promise<void> {
-  return (res: Response | Promise<Response>) =>
-    respondWith(res).catch(() => {
+  return (res: Response | Promise<Response>) => {
+    return respondWith(res).catch(() => {
       // respondWith() fails when the connection has already been closed, or there is some
       // other error with responding on this connection that prompts us to
       // close it and open a new connection.
@@ -88,6 +88,7 @@ function respondWithWrapper(
         // Connection has already been closed.
       }
     });
+  };
 }
 
 async function handleFullServer<TState>(
@@ -97,7 +98,9 @@ async function handleFullServer<TState>(
 ) {
   const requests = Deno.serveHttp(conn);
   for await (const request of requests) {
-    const respondWith = respondWithWrapper(request.respondWith, conn);
+    // const respondWith = (res: Response | Promise<Response>): Promise<void> => {
+    //   return respondWithWrapper(request.respondWith, conn)(res);
+    // };
 
     metadata.container.register(SERVER_REQUEST, { useValue: request });
     const context = metadata.container.resolve<HttpContext<TState>>(
@@ -109,6 +112,12 @@ async function handleFullServer<TState>(
       metadata.middlewares,
       context.request.parserUrl.pathname,
     );
+
+    // TODO: we need to confirm this doesn't hurt performance as we include context and middleware in the handler, especially memory leak
+    const respondWith = async (res?: Response | Promise<Response>): Promise<void> => {
+      await runCorePostRequestMiddlewares(middlewares, context);
+      return await respondWithWrapper(request.respondWith, conn)(getResponse(context.response.getMergedResult()));
+    };
 
     try {
       // Resolve every pre middleware
@@ -122,7 +131,7 @@ async function handleFullServer<TState>(
         respondWith(
           getResponse(context.response.getMergedResult()),
         );
-        await runCorePostRequestMiddlewares(middlewares, context);
+        // await runCorePostRequestMiddlewares(middlewares, context);
         continue;
       }
 
@@ -133,7 +142,7 @@ async function handleFullServer<TState>(
         respondWith(
           getResponse(context.response.getMergedResult()),
         );
-        await runCorePostRequestMiddlewares(middlewares, context);
+        // await runCorePostRequestMiddlewares(middlewares, context);
         continue;
       }
 
@@ -148,9 +157,10 @@ async function handleFullServer<TState>(
 
         // try resolve hooks, resolveHooks returns true if isImmediately is set on hook
         if (
-          hasHooks(hooks) && await resolveHooks(context, "onPreAction", hooks)
+          hasHooks(hooks) && await resolveHooks(context, 'onPreAction', respondWith, hooks)
         ) {
-          await runCorePostRequestMiddlewares(middlewares, context);
+          // await runCorePostRequestMiddlewares(middlewares, context);
+          // respondWith(getResponse(context.response.getMergedResult()));
           continue;
         }
 
@@ -172,10 +182,10 @@ async function handleFullServer<TState>(
           // try resolve hooks
           if (
             hasHooks(hooks) &&
-            hasHooksAction("onCatchAction", hooks) &&
-            await resolveHooks(context, "onCatchAction", hooks)
+            hasHooksAction('onCatchAction', hooks) &&
+            await resolveHooks(context, 'onCatchAction', respondWith, hooks)
           ) {
-            await runCorePostRequestMiddlewares(middlewares, context);
+            // await runCorePostRequestMiddlewares(middlewares, context);
             continue;
           } else {
             // Resolve every post middleware if error was not caught
@@ -183,7 +193,7 @@ async function handleFullServer<TState>(
 
             if (context.response.isImmediately()) {
               respondWith(getResponse(context.response.getMergedResult()));
-              await runCorePostRequestMiddlewares(middlewares, context);
+              // await runCorePostRequestMiddlewares(middlewares, context);
               continue;
             }
 
@@ -194,16 +204,16 @@ async function handleFullServer<TState>(
         // try resolve hooks
         if (
           hasHooks(hooks) &&
-          await resolveHooks(context, "onPostAction", hooks)
+          await resolveHooks(context, 'onPostAction', respondWith, hooks)
         ) {
-          await runCorePostRequestMiddlewares(middlewares, context);
+          // await runCorePostRequestMiddlewares(middlewares, context);
           continue;
         }
       }
 
       if (context.response.isImmediately()) {
         respondWith(getResponse(context.response.getMergedResult()));
-        await runCorePostRequestMiddlewares(middlewares, context);
+        // await runCorePostRequestMiddlewares(middlewares, context);
         continue;
       }
 
@@ -213,7 +223,7 @@ async function handleFullServer<TState>(
       // post request middlewares can set isImmediately to override default 404 response
       if (context.response.isImmediately()) {
         respondWith(getResponse(context.response.getMergedResult()));
-        await runCorePostRequestMiddlewares(middlewares, context);
+        // await runCorePostRequestMiddlewares(middlewares, context);
         continue;
       }
 
@@ -221,26 +231,26 @@ async function handleFullServer<TState>(
         context.response.result = notFoundAction();
 
         respondWith(getResponse(context.response.getMergedResult()));
-        await runCorePostRequestMiddlewares(middlewares, context);
+        // await runCorePostRequestMiddlewares(middlewares, context);
         continue;
       }
 
       respondWith(getResponse(context.response.getMergedResult()));
-      await runCorePostRequestMiddlewares(middlewares, context);
+      // await runCorePostRequestMiddlewares(middlewares, context);
     } catch (error) {
       if (app.globalErrorHandler) {
         app.globalErrorHandler(context, error);
 
         if (context.response.isImmediately()) {
+          // await runCorePostRequestMiddlewares(middlewares, context);
           respondWith(getResponse(context.response.getMergedResult()));
-          await runCorePostRequestMiddlewares(middlewares, context);
           continue;
         }
       }
 
       if (context.response.isImmediately()) {
         respondWith(getResponse(context.response.getMergedResult()));
-        await runCorePostRequestMiddlewares(middlewares, context);
+        // await runCorePostRequestMiddlewares(middlewares, context);
         continue;
       }
 
@@ -249,7 +259,7 @@ async function handleFullServer<TState>(
       }
 
       respondWith(getResponse(Content(error, error.httpCode || 500)));
-      await runCorePostRequestMiddlewares(middlewares, context);
+      // await runCorePostRequestMiddlewares(middlewares, context);
     }
   }
 }
